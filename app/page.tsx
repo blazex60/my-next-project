1e98c6fedb43a90e07375f3cb9551f4e6f40a905
= <=>
2025-11-15 04:50:13 +0000

    スタイルを適用してページコンポーネントのクラス名を修正

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
   const name:string = "世界";
   return (
     <div>
-      <h1 className="title">テクノロジーの力で{name}を変える</h1>
-      <p className='description'>私達は市場をリードしているグローバルカンパニーです</p>
+      <h1 className={Styles.title}>テクノロジーの力で{name}を変える</h1>
+      <p className={Styles.description}>私達は市場をリードしているグローバルカンパニーです</p>
     </div>
   )
 }
