e3a1f7f9bd0a095a87f851aee341c4b9ec4372e8
= <=>
2025-11-29 07:27:35 +0000

    ヘッダーとフッターコンポーネントを追加し、レイアウトを整えました。フッターのスタイル用のCSSファイルも作成しました。

---
+++ app/_components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./index.module.css"
+
+export default function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <nav className={styles.nav}>
+                
+            </nav>
+        </footer>
+    )
+}
