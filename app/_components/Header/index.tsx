c122fe1286911134bc8bc486a67d5712bf50e2cf
= <=>
2025-12-06 07:28:30 +0000

    feat: ヘッダーにメニューコンポーネントを追加し、ヒーローセクションを実装。スタイルを定義したCSSファイルを作成

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./index.module.css";
 import Link from "next/link";
+import Menu from "../Menu";
 
 export default function Header() {
     return (
@@ -8,19 +9,7 @@ export default function Header() {
             <Link href="/" className={styles.logoLink}>
                 <Image src="/logo.svg" alt="SIMPLE" className={styles.logo} width={348} height={133} priority/>
             </Link>
-            <nav className={styles.nav}>
-                <ul className={styles.items}>
-                    <li>
-                        <Link href="/news">ニュース</Link>
-                    </li>
-                    <li>
-                        <Link href="/members">メンバー</Link>
-                    </li>
-                    <li>
-                        <Link href="/contact">お問い合わせ</Link>
-                    </li>
-                </ul>
-            </nav>
+            <Menu/>
         </header>
     )
 }
