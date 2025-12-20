e3a57da188d8a2f4860878911135519ceb1591e5
= <=>
2025-12-20 07:14:53 +0000

    feat: カテゴリ詳細取得機能を追加し、ニュースリストにページネーションを実装。スタイルを定義したCSSファイルを作成

--- app/_libs/utils.ts
+++ app/_libs/utils.ts
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
+import { MicroCMSClient, MicroCMSQueries } from "microcms-js-sdk";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
